0c5f987235cef22f5606f4ccc43b8dba403106ee
saigirishgilly98 <saigirishgilly98@gmail.com>
2020-09-30 18:45:08 +0530

    Initial commit

---
+++ tester.py
@@ -0,0 +1,66 @@
+#
+# Assignment Interface
+#
+
+from pymongo import MongoClient
+import json
+import traceback
+import Assignment_Interface as Assignment
+
+DATABASE_NAME = "ddsassignment"
+COLLECTION_NAME = "businessCollection"
+CITY_TO_SEARCH = "tempe"
+MAX_DISTANCE = 100
+CATEGORIES_TO_SEARCH = ["Fashion", "Food", "Cafes"]
+MY_LOCATION = ["40", "50"]  # [LATITUDE, LONGITUDE]
+SAVE_LOCATION_1 = "findBusinessBasedOnCity.txt"
+SAVE_LOCATION_2 = "findBusinessBasedOnLocation.txt"
+
+
+def loadBusinessTable(fileName, collection):
+    try:
+        page = open(fileName, "r")
+        parsedJson = json.loads(page.read())
+        for oneItem in parsedJson["BusinessRecords"]:
+            collection.insert(oneItem)
+            # print(oneItem)
+    except Exception as e:
+        traceback.print_exc()
+
+
+def deleteDB(client, databaseName):
+    client.drop_database(databaseName)
+
+
+if __name__ == '__main__':
+    try:
+        # Getting Connection from MongoDB
+        conn = MongoClient('mongodb://localhost:27017/')
+
+        # Creating a New DB in MongoDB
+        print("Creating database in MongoDB named as " + DATABASE_NAME)
+        database = conn[DATABASE_NAME]
+
+        # Creating a collection named businessCollection in MongoDB
+        print("Creating a collection in " + DATABASE_NAME + " named as " + COLLECTION_NAME)
+        collection = database[COLLECTION_NAME]
+
+        # Loading BusinessCollection from a json file to MongoDB
+        print("Loading testData.json file in the " + COLLECTION_NAME + " present inside " + DATABASE_NAME)
+        loadBusinessTable("testData.json", collection)
+
+        # Finding All Business name and address(full_address, city and state) present in CITY_TO_SEARCH
+        print("Executing FindBusinessBasedOnCity function")
+        Assignment.FindBusinessBasedOnCity(CITY_TO_SEARCH, SAVE_LOCATION_1, collection)
+
+        # Finding All Business name and address(full_address, city and state) present in radius of MY_LOCATION for CATEGORIES_TO_SEARCH
+        print("Executing FindBusinessBasedOnLocation function")
+        Assignment.FindBusinessBasedOnLocation(CATEGORIES_TO_SEARCH, MY_LOCATION, MAX_DISTANCE, SAVE_LOCATION_2,
+                                                collection)
+
+        # Delete database
+        # deleteDB(conn, DATABASE_NAME)
+        conn.close()
+
+    except Exception as detail:
+        traceback.print_exc()
